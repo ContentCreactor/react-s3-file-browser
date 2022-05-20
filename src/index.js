861840e7148440dbd5ed6c545bc9c2d3ab6e52c3
Content Creactor <creactorcontent@gmail.com>
2022-05-20 17:07:01 +0200

    added setup for cloud run

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { configure } from 'mobx';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AWS from 'aws-sdk';
 import App from './components/App';
 import * as s3ConfigDeterminator from './services/s3ConfigDeterminator';
 import S3DirectoryListBuilder from './services/S3DirectoryListBuilder';
@@ -24,44 +23,19 @@ async function loadData() {
   try {
     let bucketName, objectUrlBase, basePath, contents;
 
-    // if (window.s3Config) {
-    //   let forcePathStyle;
-    //   ({ bucketName, forcePathStyle, objectUrlBase, basePath } = s3ConfigDeterminator.fromS3Config(window.s3Config));
-    //     const s3 = new AWS.S3({ params: { Bucket: bucketName }, s3ForcePathStyle: forcePathStyle });
-    //     const data = await s3.makeUnauthenticatedRequest('listObjectsV2').promise();
-    //     contents = data.Contents;
-    // } else
-    // if (process.env.NODE_ENV !== 'production') {
-    const splitPath = window.location.pathname.split('/');
-    if (splitPath.length > 2 && splitPath[1] === 'generic') {
-      bucketName = splitPath[2];
-      objectUrlBase = `https://${bucketName}.s3.amazonaws.com`;
-      basePath = `/generic/${bucketName}`;
-      const s3 = new AWS.S3({ params: { Bucket: bucketName } });
-      const data = await s3.makeUnauthenticatedRequest('listObjectsV2').promise();
-      contents = data.Contents;
-    } else {
-      console.log('using fake s3 data');
-      bucketName = 'example-bucket';
-      objectUrlBase = `https://${bucketName}.s3.amazonaws.com`;
-      basePath = '/';
-      const data = [
-        { Key: 'foo1.file', Size: 1, LastModified: new Date('2000-01-01 00:00:00 +0000') },
-        { Key: 'foo2/bar1.file', Size: 2, LastModified: new Date('2000-01-01 00:00:01 +0000') },
-        { Key: 'foo2/bar2/baz1.file', Size: 3, LastModified: new Date('2000-01-01 00:00:02 +0000') },
-        { Key: 'foo2/bar2/baz2.file', Size: 4, LastModified: new Date('2000-01-01 00:00:00 +0000') },
-        { Key: 'foo2/bar3/baz3.file', Size: 5, LastModified: new Date('2000-01-01 00:00:01 +0000') },
-      ];
-      contents = await new Promise((resolve, reject) => setTimeout(() => resolve(data), 500));
-    }
-    // } else {
-    //   let forcePathStyle;
-    //   ({ bucketName, forcePathStyle, objectUrlBase, basePath } = s3ConfigDeterminator.fromHostPath({ hostname: window.location.hostname, pathname: window.location.pathname }));
+    console.log('using fake s3 data');
+    bucketName = 'example-bucket';
+    objectUrlBase = `https://${bucketName}.s3.amazonaws.com`;
+    basePath = '/';
+    const data = [
+      { Key: 'foo1.file', Size: 1, LastModified: new Date('2000-01-01 00:00:00 +0000') },
+      { Key: 'foo2/bar1.file', Size: 2, LastModified: new Date('2000-01-01 00:00:01 +0000') },
+      { Key: 'foo2/bar2/baz1.file', Size: 3, LastModified: new Date('2000-01-01 00:00:02 +0000') },
+      { Key: 'foo2/bar2/baz2.file', Size: 4, LastModified: new Date('2000-01-01 00:00:00 +0000') },
+      { Key: 'foo2/bar3/baz3.file', Size: 5, LastModified: new Date('2000-01-01 00:00:01 +0000') },
+    ];
+    contents = await new Promise((resolve, reject) => setTimeout(() => resolve(data), 500));
 
-    //   const s3 = new AWS.S3({ params: { Bucket: bucketName }, s3ForcePathStyle: forcePathStyle });
-    //   const data = await s3.makeUnauthenticatedRequest('listObjectsV2').promise();
-    //   contents = data.Contents;
-    // }
 
     const treeBuilder = S3DirectoryListBuilder(bucketName, objectUrlBase);
 
@@ -77,6 +51,7 @@ async function loadData() {
     }
   }
 }
+
 
 loadData();
 
